44b32437912fc68bbe45cbcb0a756c2dc160df9c
Srinidhi Murali <z5687883>
2026-04-03 20:01:16 +1100

    fixed stuff 2

--- data/agents.ts
+++ data/agents.ts
@@ -47,8 +47,8 @@ export const domains = [
   },
 
   {
-    name: "Tax",
-    slug: "tax",
+    name: "Taxation",
+    slug: "taxation",
     agents: [
       { name: "TaxGPT", url: "https://www.taxgpt.com/" },
       { name: "Empathetic AI", url: "https://empathetic-ai.com/home" },
